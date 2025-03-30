c20efcceb2241768529d8140c4301249eec15249
milad <miladsoltanidev@gmail.com>
2025-03-30 17:02:09 +0200

    https fix the problem?!

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'http://primitiveint.github.io',
+	site: 'https://primitiveint.github.io',
 	integrations: [mdx(), sitemap()],
 });
