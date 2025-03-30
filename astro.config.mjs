713b91ea52dcb4519202a713007239fb94876328
milad <miladsoltanidev@gmail.com>
2025-03-30 19:35:17 +0200

    archived old files, started new ones using tailwind ui

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://primitiveint.com',
-	base: '/',
-	integrations: [mdx(), sitemap()],
+  site: 'https://primitiveint.com',
+  base: '/',
+  integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'PrimitiveInt';
+export const SITE_TITLE = 'Primitive Int';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
