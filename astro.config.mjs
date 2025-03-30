648da92b7c7e1fffd5bae766d5ac05d4e4231005
milad <miladsoltanidev@gmail.com>
2025-03-30 17:10:47 +0200

    changed base

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://primitiveint.github.io',
-	base: '/milsoltani',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 });
